1a5ac062a5d1e25e0e43defd5a66c5e87cd8e51c
fedi <fedirouatbi@gmail.com>
2024-05-01 12:32:40 +0100

    add amdin seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -29,53 +29,51 @@ async function main() {
     });
     console.log(`Role ${role.name} with code ${role.role_code} upserted!`);
   }
+
+  const adminsData: {
+    first_name: string;
+    last_name: string;
+    user_name: string;
+    email: string;
+  }[] = [
+    {
+      email: 'fedirouatbi@gmail.com',
+      first_name: 'Fedi',
+      last_name: 'Rouatbi',
+      user_name: 'fedirouatbi',
+    },
+  ];
+
+  for (let i = 0; i < adminsData.length; i++) {
+    const adminData = adminsData[i];
+    const password = '123456';
+    const password_hash = await bcrypt.hash(password, 1);
+    console.log(`Creating admin ${adminData.user_name} ...`);
+    const admin = await prisma.user.upsert({
+      where: { user_name: adminData.user_name },
+      update: {},
+      create: {
+        first_name: adminData.first_name,
+        last_name: adminData.last_name,
+        user_name: adminData.user_name,
+        password_hash,
+        email: adminData.email,
+        roles_on_users: {
+          create: [
+            {
+              status: RoleOnUserStatusEnum.ACTIVE,
+              role: { connect: { role_code: RoleCodeEnum.ADMIN } },
+            },
+          ],
+        },
+      },
+    });
+    console.log(
+      `Admin ${admin.first_name} with username ${admin.user_name} upserted!`,
+    );
+    console.log('Finished seeds!');
+  }
 }
-
-//   const adminsData: {
-//     first_name: string;
-//     last_name: string;
-//     user_name: string;
-//     email: string;
-//   }[] = [
-//     {
-//       email: 'fedirouatbi@gmail.com',
-//       first_name: 'Fedi',
-//       last_name: 'Rouatbi',
-//       user_name: 'fedirouatbi',
-//     },
-//   ];
-
-//   for (let i = 0; i < adminsData.length; i++) {
-//     const adminData = adminsData[i];
-//     const password = '123456' + adminData.user_name;
-//     const password_hash = await bcrypt.hash(password, 1);
-//     console.log(`Creating admin ${adminData.user_name} ...`);
-//     const admin = await prisma.user.upsert({
-//       where: { user_name: adminData.user_name },
-//       update: {},
-//       create: {
-//         first_name: adminData.first_name,
-//         last_name: adminData.last_name,
-//         user_name: adminData.user_name,
-//         force_reset_password: true,
-//         password_hash,
-//         email: { create: { email_value: adminData.email } },
-//         roles_on_users: {
-//           create: [
-//             {
-//               status: RoleOnUserStatusEnum.ACTIVE,
-//               role: { connect: { role_code: RoleCodeEnum.ADMIN } },
-//             },
-//           ],
-//         },
-//       },
-//     });
-//     console.log(
-//       `Admin ${admin.first_name} with username ${admin.user_name} upserted!`,
-//     );
-//   }
-//   console.log('Finished seeds!');
-// }
 main()
   .then(async () => {
     await prisma.$disconnect();
